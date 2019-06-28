38733b20b0fad2fd57bb183dc405ee26b53a8afa
Thuy Dang Tran <thuydt@greenglobal.vn>
2019-06-28 23:21:10 +0700

    Singleton

---
+++ SingletonPatternEx/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SingletonPatternEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
