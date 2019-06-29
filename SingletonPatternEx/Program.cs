483e175ce3bde0de229eac31e5d8f91a61dd581c
Thuy Dang Tran <thuydt@greenglobal.vn>
2019-06-29 13:10:19 +0700

    Singleton Parttern

--- SingletonPatternEx/Program.cs
+++ SingletonPatternEx/Program.cs
@@ -6,7 +6,20 @@ namespace SingletonPatternEx
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("***Singleton Pattern Demo***\n");
+            Console.WriteLine("Trying to create instance s1.");
+            Singleton s1 = Singleton.Instance;
+            Singleton s2 = Singleton.Instance;
+            if (s1 == s2)
+            {
+                Console.WriteLine("Only one instance exists.");
+            }
+            else
+            {
+                Console.WriteLine("Different instances exist.");
+            }
+
+
         }
     }
 }
---
+++ SingletonPatternEx/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonPatternEx
+{
+    public sealed class Singleton
+    {
+        private static readonly Singleton instance = new Singleton();
+        private int numberOfInstance = 0;
+        public static int MyInt = 25;
+
+        public static Singleton Instance
+        {
+            get
+            {
+                Console.WriteLine("We already have an instance now.Use it.");
+                return instance;
+            }
+        }
+        private Singleton()
+        {
+            Console.WriteLine("Instantiating inside the private constructor");
+            numberOfInstance++;
+            Console.WriteLine("Number of instances ={0}", numberOfInstance);
+        }
+    }
+}
